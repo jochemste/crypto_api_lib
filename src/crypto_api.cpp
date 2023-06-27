649e4bb998cea9e7977eaa197ac244faa788633a
jochemste <jochemstevense@protonmail.com>
2023-06-27 16:59:37 +0200

    Added googletest and boost libraries
    Signed-off-by: jochemste <jochemstevense@protonmail.com>

---
+++ src/crypto_api.cpp
@@ -0,0 +1,5 @@
+#include <boost/beast/websocket.hpp>
+
+namespace beast = boost::beast;
+
+int main(int argc, char **argv) { return 0; }
---
+++ test/test_crypto_api.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+class CryptoAPITest : public ::testing::Test {};
+
+TEST_F(CryptoAPITest, DoSomething) { ASSERT_EQ(0, 0); }
