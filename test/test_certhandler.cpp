17eeb4ca51d510cc1261d76c065382702ab046d5
jochemste <jochemstevense@protonmail.com>
2023-07-09 12:06:17 +0200

    Adjusted test to be more readable

--- test/test_certhandler.cpp
+++ test/test_certhandler.cpp
@@ -2,6 +2,7 @@
 
 #include "certhandler.hpp"
 
+#include <iostream>
 #include <string>
 
 class CerthandlerTest : public ::testing::Test {};
@@ -9,7 +10,9 @@ class CerthandlerTest : public ::testing::Test {};
 TEST_F(CerthandlerTest, NoErrorOnGetCert) {
   Cert::CerthandlerFactory ch_factory;
   Cert::ICertHandler *ch = ch_factory.create();
+  std::string ca_certs;
 
-  std::string output;
-  ch->get_ca_certificates(output);
+  ASSERT_NO_THROW(ch->get_ca_certificates(ca_certs));
+
+  ASSERT_FALSE(ca_certs.empty());
 }
